1da99e9e5aabfa25b4697f63555e22c2a7c595ae
Kai T. Ohlhus <k.ohlhus@gmail.com>
2020-08-31 15:28:38 +0900

    Add TravisCI.

---
+++ assets/travis/run_yamllint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# If no file is given, check all packages.
+
+if [ -z "$1" ]; then
+  files="`cd "$DIR/../../package" && pwd`/*"
+else
+  files=$1
+fi
+
+# Check YAML files in the "package" directory of this repository.
+
+yamllint -c $DIR/yamllint.yaml $files
